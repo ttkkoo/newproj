0c85522fffdc93d6df0a7a8fbf4493e440986957
bingguo1 <changethespace@gmail.com>
2021-11-02 13:35:02 -0400

    add new after setting the rule

--- a.C
+++ a.C
@@ -4,4 +4,5 @@ int main(){
     std::cout<<" hello world"<<std::endl;
     std::cout<<" edit from macbookpro"<<std::endl;
     std::cout<<" add another another line from macbookpro"<<std::endl;
+    std::cout<<"new lines after applying the restriction rule"<<std::endl;
 }
